ee17f1885eb56e261615e203a2488a291e05e1e0
Michael Blanchard <[mblanch08@gmail.com]>
2018-07-19 02:45:39 +0000

    restart

--- application.py
+++ application.py
@@ -1,15 +1,42 @@
+# import os
+# import requests
+
+# from flask import Flask, jsonify, render_template, request, url_for
+# from flask_socketio import SocketIO, emit
+
+# app = Flask(__name__)
+# app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+# socketio = SocketIO(app)
+
+
+# users = []
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+# @app.route("/populate", methods=["POST"])
+# def populate():
+#     print(channels)
+#     return jsonify(channels)
+
+# @socketio.on("add channel")
+# def add(data):
+#      name = data["name"]
+#      channels.append(name)
+#      emit("announce channels", {"name": name}, broadcast = True)
 import os
 import requests
+import sys
 
-from flask import Flask, jsonify, render_template, request, url_for
+from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-channels = ["test1", "test2", "test3", "test4"]
-users = []
+channels = []
 
 @app.route("/")
 def index():
@@ -17,11 +44,11 @@ def index():
 
 @app.route("/populate", methods=["POST"])
 def populate():
-    print(channels)
     return jsonify(channels)
 
 @socketio.on("add channel")
 def add(data):
-     name = data["name"]
-     channels.append(name)
-     emit("announce channels", {"name": name}, broadcast = True)
+    name = data["name"]
+    channels.append(name)
+    print(f"{name}", file=sys.stderr)
+    emit("announce channels", {"name": name}, broadcast = True)
--- static/index.js
+++ static/index.js
@@ -1,71 +1,131 @@
 
+// document.addEventListener('DOMContentLoaded', () => {
+//     alert("hello")
+//     //load active channels
+//     onStart();
+
+//     //get cached username
+//     var username = localStorage.getItem('username');
+
+//     //if user does not have name cached
+//     if (username == 'null') {
+//         console.log("anything");
+//         //create new username
+//         document.querySelector('#form').onsubmit = () => {
+//             localStorage.setItem('username', username);
+//             addToList('#users_list', '#username', 'list-group-item');
+//             var myNode = document.getElementById("form");
+//             while (myNode.firstChild) {
+//                 myNode.removeChild(myNode.firstChild);
+//             }
+//             return false;
+//         };
+//     //else in case of username already exist welcome user and remove
+//     //username entry nodes
+//     } else {
+//         var myNode = document.getElementById("form");
+//         while (myNode.firstChild) {
+//             myNode.removeChild(myNode.firstChild);
+//         }
+//         addToList('#users_list', username, 'list-group-item');
+//     }
+
+//     //OLD CODE PROBABLY NO USEFUL
+//     //user input for new channel creation
+//     // document.querySelector('#form_chat').onsubmit = () => {
+//     //     addToList('#chat_list', '#chatroom_name', 'list-group-item');
+//     //     return false;
+//     // };
+
+
+//     //connect to socket
+//     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+//     //when new chatroom name submitted emit to 'add channel' new channel name
+//     socket.on('connect', () => {
+//         document.querySelector('#form').onsubmit = () => {
+//             const name = '#chatroom_name';
+//             socket.emit('add channel', {'name': name});
+//             return false;
+//         };
+//     });
+
+//     // //recieve a new channels created
+//     socket.on('announce channels'), data => {
+//         populateChannels(data.name);
+//     };
+
+// });
+
+// //####################################################################################
+// //####################################################################################
+
+// function populateChannels(contents) {
+//     let li = document.createElement('li');
+//     li.className = 'list-group-item';
+//     li.innerHTML = {"contents": contents};
+//     document.querySelector('#chat_list').append(li);
+// }
+
+// //adds new elements to list in index.html
+// function addToList(list, value, className) {
+//     let newElement = document.querySelector(value).value;
+//     let li = document.createElement('li');
+//     li.className = className;
+//     li.innerHTML = newElement;
+//     document.querySelector(list).append(li);
+// }
+
+// //load new channels on start
+// function onStart() {
+//     const request = new XMLHttpRequest();
+//     request.open('POST', '/populate');
+//     request.onload = () => {
+//         const data = JSON.parse(request.responseText);
+//         data.forEach(populateChannels);
+//     };
+// }
+
+
+
+    if (!localStorage.getItem('username'))
+            localStorage.setItem('username', null);
+
+
 document.addEventListener('DOMContentLoaded', () => {
+    var username = localStorage.getItem('username')
 
-    //load active channels
-    onStart();
-
-    //get cached username
-    var username = localStorage.getItem('username');
-
-    //if user does not have name cached
-    if (username == 'null') {
-        //create new username
-        document.querySelector('#form').onsubmit = () => {
-            localStorage.setItem('username', username);
+    document.querySelector('#form').onsubmit = function() {
+        localStorage.setItem('username', username);
             addToList('#users_list', '#username', 'list-group-item');
             var myNode = document.getElementById("form");
             while (myNode.firstChild) {
                 myNode.removeChild(myNode.firstChild);
             }
-            return false;
-        };
-    //else in case of username already exist welcome user and remove
-    //username entry nodes
-    } else {
-        var myNode = document.getElementById("form");
-        while (myNode.firstChild) {
-            myNode.removeChild(myNode.firstChild);
-        }
-        addToList('#users_list', username, 'list-group-item')
-        return false;
-    }
-
-    //user input for new channel creation
-    // document.querySelector('#form_chat').onsubmit = () => {
-    //     addToList('#chat_list', '#chatroom_name', 'list-group-item');
-    //     return false;
-    // };
+        return false
+    };
 
 
-    //connect to sockot
+    // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
     //when new chatroom name submitted emit to 'add channel' new channel name
-    socket.on('connect', () => {
-        document.querySelector('#form').onsubmit = () => {
-            const name = '#chatroom_name';
-            socket.emit('add channel', {'name': name});
-            return false;
-        };
-    });
 
-    //recieve a new channels created
+    document.querySelector('#form_chat').onsubmit = () => {
+        alert("new chat submitted")
+        var name = 'chatroom_name';
+        socket.emit('add channel', {'name': name});
+        return false;
+    };
+
+
+    // //recieve a new channels created
     socket.on('announce channels'), data => {
+        alert("announce channels")
         populateChannels(data.name);
     };
 
-    return false; //not sure if this is neccessary?
 });
-
-//####################################################################################
-//####################################################################################
-
-function populateChannels(contents) {
-    let li = document.createElement('li');
-    li.className = 'list-group-item';
-    li.innerHTML = {"contents": contents};
-    document.querySelector('#chat_list').append(li);
-}
 
 //adds new elements to list in index.html
 function addToList(list, value, className) {
@@ -76,14 +136,9 @@ function addToList(list, value, className) {
     document.querySelector(list).append(li);
 }
 
-//load new channels on start
-function onStart() {
-    const request = new XMLHttpRequest();
-    request.open('POST', '/populate');
-    request.onload = () => {
-        const data = JSON.parse(request.responseText);
-        data.forEach(print);
-        data.forEach(populateChannels);
-    };
+function populateChannels(contents) {
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.innerHTML = {"contents": contents};
+    document.querySelector('#chat_list').append(li);
 }
-
